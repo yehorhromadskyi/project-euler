3c721310bdae8a5ab8ffcc6e8cc91b601d046ab2
Yehor Hromaskyi <egromad27@hotmail.com>
2018-05-18 22:17:21 +0300

    made files as scripts

--- Problem2.fsx
+++ Problem2.fsx
@@ -1,6 +1,6 @@
 
 let fib = Seq.unfold (fun state ->
     if snd state > 4000000 then None
-    else Some(fst state + snd state, (snd state, fst state + snd state))) (1,1)
+    else Some(fst state + snd state, (snd state, fst state + snd state))) (1, 1)
 
 let sumOfEvenFib = fib |> Seq.filter (fun n -> n % 2 = 0) |> Seq.sum
