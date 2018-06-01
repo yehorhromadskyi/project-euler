f81e2f29e658a5d52d86c39d094dc53907daf42d
Yehor Hromadskyi <yhrom@softserveinc.com>
2018-06-01 18:24:01 +0300

    no message

--- src/Problems/Problem2.fs
+++ src/Problems/Problem2.fs
@@ -2,8 +2,10 @@
 
 open Ranges
 
-let sumOfEvenNumbers = 
+let sumOfEvenNumbers till = 
     fibonacci
-    |> Seq.takeWhile (fun i -> i < 4. * 1000. * 1000.)
+    |> Seq.takeWhile (fun i -> i < till)
     |> Seq.filter (fun n -> n % 2. = 0.) 
     |> Seq.sum
+
+//sumOfEvenNumbers (4. * 1000. * 1000.)
