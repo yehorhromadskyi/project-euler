9a2c98a4ea10bfb969806088f26452aa466e1ed2
Yehor Hromadskyi <yhrom@softserveinc.com>
2018-06-06 18:46:09 +0300

    5

--- src/Problems.Tests/ProblemsTests.fs
+++ src/Problems.Tests/ProblemsTests.fs
@@ -17,10 +17,19 @@ module ``Problem 3 tests`` =
     let ``The largest prime factor of 13195`` () =
         Assert.Equal(29UL, largestPrimeFactor 13195UL)
 
+
 module ``Problem 4 tests`` =
     open Problem4
     
     [<Fact>]
     let ``The largest palindrome made from the product of two 2-digit numbers`` ()=
         Assert.Equal(9009, largestPalindromeBetween 9 99)
+
+
+module ``Problem 5 tests`` =
+    open Problem5
+    
+    [<Fact>]
+    let ``Smallest number that can be divided by each of the numbers from 1 to 10`` ()=
+        Assert.Equal(2520, findSmallestDivisible 10)
 
---
+++ src/Problems/Problem5.fs
@@ -0,0 +1,20 @@
+﻿module Problem5
+
+    let isEvenlyDivisible number till =  
+        let rec calculate i =
+            if i > till
+                then true
+            elif number % i = 0
+                then calculate (i + 1)
+            else false
+        calculate 2
+    
+    let findSmallestDivisible till =
+        let rec find n = 
+            if isEvenlyDivisible n till
+                then n
+            else find (n + 1)
+        find 2
+    
+    //findSmallestDivisible 20
+
