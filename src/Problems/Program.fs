05044b7aa7b7853e85cb278fcc27bc09fe345c2c
Yehor Hromaskyi <egromad27@hotmail.com>
2018-11-27 13:37:17 +0200

    13!

--- src/Problems/Program.fs
+++ src/Problems/Program.fs
@@ -2,26 +2,15 @@
 open System.Diagnostics
 open Mathematics
 
-
 [<EntryPoint>]
 let main argv =
     let sw = new Stopwatch()
     sw.Start()
-    // let warmUpCall = Problem12.findTriangleNumberWithOver 150
-    // printfn "Warm up call took %d milliseconds" sw.ElapsedMilliseconds
-    // sw.Restart()
-    let result = Problem12.findTriangleNumberWithOver 500
+    let warmUpCall = Problem13.findLargeSum
+    printfn "Warm up call took %d milliseconds" sw.ElapsedMilliseconds
+    sw.Restart()
+    let result = Problem13.findLargeSum
     printfn "Result %i in %d milliseconds" result sw.ElapsedMilliseconds
-
-    // sw.Restart()
-
-    // let result1 = Attributes.getFactors (System.Int32.MaxValue - 1)
-    // printfn "Result %i in %d milliseconds" result1.Length sw.ElapsedMilliseconds
-
-    // sw.Restart()
-
-    // let result2 = Attributes.countFactors (System.Int32.MaxValue - 1)
-    // printfn "Result2 %i in %d milliseconds" result2 sw.ElapsedMilliseconds
 
     sw.Stop()
 
